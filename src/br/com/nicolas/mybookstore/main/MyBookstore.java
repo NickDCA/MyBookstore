40bcd7604ae4758c0b6b0bc5ed60d46e08760817
nicolasdca <nicolasdavid193@gmail.com>
2024-03-13 22:51:06 -0300

    First classes

---
+++ src/br/com/nicolas/mybookstore/main/MyBookstore.java
@@ -0,0 +1,7 @@
+package br.com.nicolas.mybookstore.main;
+
+public class MyBookstore {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
